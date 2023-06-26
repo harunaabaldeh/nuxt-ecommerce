b87a1b94eec9881a5d014fc92d1cbf9f306903f3
haruna <haruna@writemedia.co.uk>
2023-06-26 17:10:53 +0000

    modified url structure

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -8,7 +8,7 @@
 
 <script setup>
 const { id } = useRoute().params;
-const uri = "https://fakestoreapi.com/products" + id;
+const uri = "https://fakestoreapi.com/products/" + id;
 
 //fetch the product
 const { data: product } = await useFetch(uri, { key: id });
